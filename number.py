73b5671612d94671260eaeb46a28ce0a2370b1ab
ABU ISHAQUE MD ISRAIL <ishaquenafi100@gmail.com>
2026-07-09 03:54:30 -0400

    practice

---
+++ number.py
@@ -0,0 +1,57 @@
+"""0
+b=3
+c=3.13
+
+print("addition:" , a+b)
+print("subtraction:" , a-b)
+print("multiplication:" , a*b)
+print("devisoin:" , a/b)
+print("modulus: " , a%b)
+print("exponentiation:" , a**b)
+print("floorL: " , a//b)
+print("bits: " , a%b)
+
+
+
+x=10
+y=3.14
+
+
+print("int to float: " , float(x))
+print("float to int :" , int(y))
+print(" int to compelx: ",  complex(x))
+
+
+
+
+import math
+
+print(math.sqrt(16))
+
+print(math.pow(4,5))
+
+print(math.sin(math.radians(90)))
+
+print(math.log10(1000))
+
+print(math.factorial(5))
+print(math.fabs(-5))
+
+print(math.floor(3.44))
+print(math.ceil(3.44))
+
+print(math.pi)
+print(math.e)
+
+num1=10
+num2=20
+
+print(math.gcd(num1,num2))
+print(math.lcm(num1,num2))
+
+ """
+
+
+
+result=10+3*2
+print("Result:", result)
