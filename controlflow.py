4dc819226207eeef7617e3af3495fc4387e64804
ABU ISHAQUE MD ISRAIL <ishaquenafi100@gmail.com>
2026-07-10 04:21:45 -0400

    if-else practice

---
+++ controlflow.py
@@ -0,0 +1,33 @@
+
+"""age=int(input("Enter your age: "))
+if age>=18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
+
+
+mark=int(input("Enter your mark: "))
+if mark<=100 and mark>=90:
+    print("Grade: A")
+    print("Excellent performance!")     
+elif mark<90 and mark>=80:
+    print("Grade: B")
+else :
+    print("Grade is not good, go and study hard")
+"""
+
+
+
+age=7
+
+has_permisson=True
+
+if age>=18:
+    if has_permisson== False:
+        print("You are eligible to vote.")
+    else:
+        print("You are not eligible to vote.")
+else:
+    print("You are not eligible ")
+
+        
