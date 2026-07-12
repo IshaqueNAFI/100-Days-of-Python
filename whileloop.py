2abef939066973c9aa8453f7c48c45196046d957
ABU ISHAQUE MD ISRAIL <ishaquenafi100@gmail.com>
2026-07-12 01:14:51 -0400

    wholeLoop

---
+++ whileloop.py
@@ -0,0 +1,66 @@
+"""fruits = ["apple", "banana", "cherry"]
+index=0
+while index < len(fruits):
+     print(fruits[index])
+     index= index + 1
+     """
+
+word= "hello"  
+index=0
+while index< len(word):
+    print(word[index])
+    index= index + 1
+    
+
+index=0
+ending=6
+while index<ending :
+
+    if index==5:
+        index= index +1
+        continue
+    print(index )
+    index= index +1
+
+
+# define a dictionary with exam results
+examResult = {
+    "math": 90,
+    "science": 80,
+    "english": 70,
+    "history": 60,
+}
+
+
+keys= list(examResult.keys())
+
+
+index=0
+while index<len(keys):
+    eachKey=keys[index]
+
+    ourPut= "subject  " + eachKey + "    marks= " + str(examResult[eachKey])
+    print(ourPut)
+    index+= 1
+
+
+
+my_set={1,3,4,4,5,6,7,5}
+my_list= list(my_set)
+
+index = 0
+
+while index < len(my_list):
+
+    print (my_list[index])
+    index=index + 1
+
+    
+           
+
+                   
+
+
+
+
+
