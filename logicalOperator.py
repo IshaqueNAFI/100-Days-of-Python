20973cfbe66a8c196658cd7f638108a14de2be0c
ABU ISHAQUE MD ISRAIL <ishaquenafi100@gmail.com>
2026-07-13 02:00:34 -0400

    logicalOperator

---
+++ logicalOperator.py
@@ -0,0 +1,42 @@
+age =20
+has_permission=True
+
+
+if age>=18 or has_permission:
+
+    print ("old enough")
+else:
+    print("you not old enough")    
+
+
+
+if not age>=22:
+    print ("you are a grown ass mannn")
+
+else:
+
+    print("you are not an adult")
+
+
+
+age=20
+has_permission=True
+is_vip=True
+
+
+if (age >=18 and has_permission==True) or is_vip :
+
+        print ("   enter the class")
+
+else:
+        print(" you are not allowed")
+
+
+
+x=8
+y=6
+
+print(x>=y)
+
+
+
