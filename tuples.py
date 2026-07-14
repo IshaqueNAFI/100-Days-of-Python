fe17d48b05f70d300a3ce1d7a642c0666328590d
ABU ISHAQUE MD ISRAIL <ishaquenafi100@gmail.com>
2026-07-14 02:48:52 -0400

    tuples.py

---
+++ tuples.py
@@ -0,0 +1,22 @@
+fruits = ("apple ", "banana", "cherry",3,2,True) 
+#fixed size
+#immmutable
+"""
+print(id(fruits))
+fruits = ("apple ", "banana" "cherry")
+print(id(fruits)"""
+
+number= fruits.count(str("banana"))
+print(number)
+
+IndexValue=fruits.index(2)
+print(IndexValue)
+
+fruitList= list(fruits)
+print(fruitList)
+
+print(fruits[1:3])
+
+for item in fruits:
+
+    print(item)
