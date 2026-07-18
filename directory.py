031e49a08b6a962a6130f1ad279b7870ae4c8565
ABU ISHAQUE MD ISRAIL <ishaquenafi100@gmail.com>
2026-07-18 01:52:31 -0400

    directory.py

---
+++ directory.py
@@ -0,0 +1,38 @@
+                                                 #create directory
+"""import os
+
+os.mkdir("new")
+
+                                                 #search all directories
+
+import os
+items=os.listdir(".")
+print(items)
+                                                #write directories
+with open("new/text.text","w") as file:
+    file.write("helllo py")
+    print("done")
+
+#rename
+import os
+os.rename("new", "new new")
+
+                                        #remove folder/directory
+import os
+os.rmdir("new new")
+
+
+                                             #remove file
+import os
+os.remove("new_new/text.file")"""
+
+
+
+import os
+
+files= os.listdir("new_new")
+
+for file in files:
+    print(file)
+
+
