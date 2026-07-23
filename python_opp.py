7d187fae2f509419a44e5576eb1572b9db40dce8
ABU ISHAQUE MD ISRAIL <ishaquenafi100@gmail.com>
2026-07-22 23:21:35 -0400

    python_opp.py

---
+++ python_opp.py
@@ -0,0 +1,23 @@
+class Myclass:
+    x=2
+    y=3
+    z=4
+    sum=z+y+x
+
+
+
+
+obj1 = Myclass()
+print(obj1.x)
+print(obj1.sum)
+
+
+obj2=Myclass()
+print(obj2.y)
+
+
+
+obj3=Myclass()
+print(obj3.z)
+
+
