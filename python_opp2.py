5cda1278211f402e0638b4083427e5f8cddda685
ABU ISHAQUE MD ISRAIL <ishaquenafi100@gmail.com>
2026-07-23 01:19:38 -0400

    python_opp2.py

---
+++ python_opp2.py
@@ -0,0 +1,41 @@
+class Myclass:
+    x=2  #classs variable
+    y=8
+    z=4
+    def addTwo(self,a,c):      
+
+        sum2 =self.x +self.y +a
+        print("sum2 result",sum2)
+
+
+    def addNew(self):
+            self.addTwo(6,6)
+
+
+
+obj = Myclass()
+obj.addTwo(10,10)
+obj.addNew()
+
+
+
+
+
+    
+"""
+
+obj1 = Myclass()
+print(obj1.x)
+print(obj1.sum)
+
+
+obj2=Myclass()
+print(obj2.y)
+
+
+
+obj3=Myclass()
+print(obj3.z)
+"""
+
+
