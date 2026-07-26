36311fb70fb0214aaf1edf0c548e858200ca1de2
ABU ISHAQUE MD ISRAIL <ishaquenafi100@gmail.com>
2026-07-26 19:08:58 -0400

    abstracoverriding_overloadin.pyt.py

--- overriding_overloadin.py
+++ overriding_overloadin.py
@@ -8,15 +8,24 @@ class Father:
 
 
 class son (Father):
-    def add (self):
-        print(self.x +self.y+100)
+    def add1 (self,a=0,b=0):                  #default argument parameter
+        print(self.x +self.y+1+b+a)
 
 
-obj = Father()
-obj.add()
+
+
+    def myMethod(self,*a):     #variable lenght argument  ( unkinted value passs)
+        print(a)
+
+
+    
+
 
 obj1= son()
-obj1.add()
-
-
+obj1.add1(4)
+obj1.add1()
+obj1.add1(3,4)
+obj1.myMethod(3,3,3,4,3,2,3,4,4,4,3,3)
+obj1.myMethod(1,3)
+obj1.myMethod()
 
