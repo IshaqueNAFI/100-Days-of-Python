8ad9979c5fdad15b756b770fb6a895850c1e89e1
ABU ISHAQUE MD ISRAIL <ishaquenafi100@gmail.com>
2026-07-29 03:16:22 -0400

    subprocess.py

---
+++ subprocess.py
@@ -0,0 +1,30 @@
+"""import subprocess
+
+result = subprocess.run(
+
+    ['python' ,'--version'],
+capture_output=True,
+text=True
+)
+
+print(result.stdout)
+
+
+import hashlib
+
+password= b"ABC12"
+
+has_object= hashlib.sha256(password)
+
+print (has_object.hexdigest())
+
+"""
+
+import csv
+
+with open ('data.csv', 'w') as file:
+
+    writer = csv.writer(file)
+
+    writer.writerow(['name' , ' nafi'])
+
